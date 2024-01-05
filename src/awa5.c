9eb79946d96853d18608595f25f0ab1cacba81a7
Stark Night <starknightawawa@gmail.com>
2024-01-05 15:52:34 +0100

    Read the compiled file header

--- src/awa5.c
+++ src/awa5.c
@@ -1,6 +1,88 @@
 #include "config.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+struct Header {
+     int8_t magic[8];
+     uint32_t label_num[2];
+     uint32_t labels[256];
+     uint32_t code_size;
+     uint32_t extra_flags;
+     uint32_t cursor;
+};
+
 int
-main(int argc, char *arv[]) {
+main(int argc, char *argv[]) {
+     if (argc < 2) {
+          fprintf(stderr, "%s %s\nUsage: %s FILE\n", argv[0], PACKAGE_VERSION, argv[0]);
+          return 1;
+     }
+
+     int input_fd = open(argv[1], O_RDONLY);
+     if (-1 == input_fd) {
+          fprintf(stderr, "no file %s\n", argv[1]);
+          return 1;
+     }
+
+     struct stat input_info = { 0 };
+     if (-1 == fstat(input_fd, &input_info)) {
+          fprintf(stderr, "stat failed\n");
+          close(input_fd);
+          return 1;
+     }
+
+     if (24 > input_info.st_size) {
+          // 8 bytes magic
+          // 4 + 4 bytes labels
+          // 4 bytes code segment size
+          // 4 bytes extra flags
+          fprintf(stderr, "malformed file\n");
+          close(input_fd);
+          return 1;
+     }
+
+     void *input_mmap =
+          mmap(NULL, input_info.st_size, PROT_READ, MAP_PRIVATE, input_fd, 0);
+     if (MAP_FAILED == input_mmap) {
+          fprintf(stderr, "mmap failed\n");
+          close(input_fd);
+          return 1;
+     }
+
+     // read the file header.
+     struct Header file_header = { 0 };
+
+     memcpy(file_header.magic, input_mmap + file_header.cursor, 8);
+     file_header.cursor = file_header.cursor + 8;
+
+     memcpy(file_header.label_num, input_mmap + file_header.cursor, 2 * sizeof(uint32_t));
+     file_header.label_num[0] = ntohl(file_header.label_num[0]);
+     file_header.label_num[1] = ntohl(file_header.label_num[1]);
+     file_header.cursor = file_header.cursor + (2 * sizeof(uint32_t));
+
+     for (uint32_t i=0; i<file_header.label_num[0]; ++i) {
+          uint32_t offset = ntohl(((uint32_t *)(input_mmap + file_header.cursor))[0]);
+          uint32_t index = ntohl(((uint32_t *)(input_mmap + file_header.cursor))[1]);
+
+          file_header.cursor = file_header.cursor + (2 * sizeof(uint32_t));
+
+          file_header.labels[index] = offset;
+     }
+
+     file_header.code_size = ntohl(((uint32_t *)(input_mmap + file_header.cursor))[0]);
+     file_header.extra_flags = ntohl(((uint32_t *)(input_mmap + file_header.cursor))[1]);
+     file_header.cursor = file_header.cursor + (2 * sizeof(uint32_t));
+
+     munmap(input_mmap, input_info.st_size);
+     close(input_fd);
+
      return 0;
 }
